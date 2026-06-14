8c120a60179bd416e286f75a096f1d583a3e9d2c
Norge Cruz <ncmanager89@gmail.com>
2026-06-14 11:23:46 -0300

    Initial commit

--- main.c
+++ main.c
@@ -38,100 +38,11 @@ void back();
 
 int main()
 {
-/*int size = 4;
-int tope = size - 1;
-int tope = 3;
 
-#antes
-#						   0        1          2         3
-#struct viajeros v[4] = ["Reina", "Norge", "Nelson", "Micaela"];
-#y quiero insertar a Niurisbel en la posicion 1
-#despues
-tope++; tope=4;
-#==========primera corrida==========
-#tope = 4;
-#pos = 1;
-#i=tope-1;
-#i=3;
-#c.arr[i+1] = c.arr[i];
-# i >= pos
-# 3 >= 1
-#si es true
-#							 0	      1	       2         3	         4
-#struct collection c[4]= ["Reina", "Norge", "Nelson", "Micaela", "Micaela"];
-#i--
-#i=2
-
-#===========segunda corrida==========
-#int tope = 4;
-#int i = 2;
-#int pos = 1;
-#c.arr[i+1]=c.arr[i];
-#c.arr[2+1]=c.arr[2];
-#c.arr[3]=c.arr[2];
-# i >= pos
-# 2 >= 1
-#si es true
-#							 0         1          2         3         4
-#struct collection c[4]=["Reina", "Norge", "Nelson", "Nelson", "Micaela"];
-#i--
-#i=1
-
-#===========tercera corrida===========
-#int tope = 4;
-#int i = 1;
-#int pos = 1;
-# i >= pos
-# 1 >= 1
-#si es true
-#c.arr[i+1]=c.arr[i];
-#c.arr[1+1]=c.arr[1];
-#c.arr[2]=c.arr[1];
-#			 				0         1          2         3         4
-#struct collection c[4]=["Reina", "Norge", "Norge", "Nelson", "Micaela"];
-#i--
-#i=0
-
-#==============cuarta corrida=============
-int tope =4;
-#int i = 0;
-#int pos = 1;
-# i >= pos
-# 0 >= 1
-#como es false porque 0 no es mayor o igual a 1
-#entonces hasta ahi llega el for,
-#salgo del for
-#y asigno el valor en el arreglo en la posicion 1
-#c.arr[pos] = new.name;
-#c.arr[1] = "Niurisbel";
-#			  				0         1         	2         3         4
-#struct travelers v[4]= ["Reina", "Niurisbel", "Norge", "Nelson", "Micaela"];
-*/
 struct collection c;
 struct traveler newTraveler;
 c.tope= -1;
 int pos;
-
-/* probando insercion de datos fijos
-c.arr[0].id = 11;
-strcpy(c.arr[0].name, "Norge");
-strcpy(c.arr[0].lastname, "Cruz");
-c.arr[0].flight_date.day = 25;
-c.arr[0].flight_date.month = 11;
-c.arr[0].flight_date.year = 2026;
-strcpy(c.arr[0].destiny, "Italy");
-
-struct traveler newTraveler;
-newTraveler.id = 17;
-strcpy(newTraveler.name, "Niurisbel");
-strcpy(newTraveler.lastname, "Cruz");
-newTraveler.flight_date.day = 20;
-newTraveler.flight_date.month = 12;
-newTraveler.flight_date.year = 2026;
-strcpy(newTraveler.destiny, "Spain");
-*/
-
-
 
 char options = '0';
 do{
@@ -202,10 +113,9 @@ c = insert(c, newTraveler, pos);
 printf("\nEl usuario ha sido insertado\n");
 }
 back();
+break;
 
-//insert(c, newTraveler, 3, 1);
 case '3' :
-
 system(CLEAR);
 printf("Complete los siguientes datos\n");
 printf("\nID: ");
@@ -243,6 +153,7 @@ break;
 return 0;
 }
 
+//insert a traveler according to the position
 struct collection insert(struct collection c, struct traveler newTraveler, int pos){
 	c.tope++;
 	for(int i = c.tope - 1; i>=pos; i--){
@@ -251,14 +162,14 @@ struct collection insert(struct collection c, struct traveler newTraveler, int p
 	c.arr[pos] = newTraveler;
 	return c;
 }
-
+//simple way to insert a traveler
 struct collection simpleInsertion(struct collection c, struct traveler newTraveler){
 	c.arr[c.tope+1] = newTraveler;
 	c.tope++;
 	return c;
 }
 
-
+//to show the travelers
 void show(struct collection c){
 int counter = 1;
 	for(int i=0; i<= c.tope; i++){
